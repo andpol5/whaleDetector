c5b19db605fd536f3098505a6c2dccbe60f493d3
andpol5 <and.pol5@gmail.com>
2015-12-09 22:28:30 +0100

    End of day changes

--- PeterRichardLyndon/trainWhales.py
+++ PeterRichardLyndon/trainWhales.py
@@ -32,7 +32,7 @@ sess = tf.InteractiveSession()
 # Constants 
 nClasses = 3
 imageSize = 128*128
-batchSize = 12
+batchSize = 37
 
 # The size of the images is 256x256
 x = tf.placeholder("float", shape=[None, imageSize])
@@ -87,17 +87,23 @@ b_fc2 = bias_variable([nClasses])
 y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
 # Load the dataset
-dataset = Dataset.Dataset('imgs', 'whales.csv')
+dataset = Dataset.Dataset('imgs', 'whales.csv', sess)
 
 # Train and eval the model
-cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv))
-train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
-#train_step = tf.train.AdamOptimizer(1e-3).minimize(cross_entropy)
+#cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv))
+#cross_entropy = -tf.reduce_sum(y_*tf.log(tf.clip_by_value(y_conv,1e-10,1.0)))
+l2_loss = tf.nn.l2_loss(tf.clip_by_value(y_conv,1e-10,1.0))
+
+#train_step = tf.train.GradientDescentOptimizer(0.1).minimize(cross_entropy)
+#train_step = tf.train.GradientDescentOptimizer(0.5).minimize(l2_loss)
+#train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+train_step = tf.train.AdamOptimizer(1e-4).minimize(l2_loss)
+
 correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 sess.run(tf.initialize_all_variables())
 
-for i in xrange(100):
+for i in xrange(300):
    step_start = time.time()
    batch = dataset.get_batch(batchSize)
    labels = batch[1]
@@ -106,22 +112,31 @@ for i in xrange(100):
    yTrain = np.zeros((batchSize, nClasses))
    for j in xrange(batchSize):
       yTrain[j][ int(labels[j]) ] = 1
-   yTrain = yTrain.tolist()
    
    if i%5 == 0:
-      train_accuracy = accuracy.eval(feed_dict={ x:batch[0], y_: yTrain, keep_prob: 1.0})
-      print "\b**step %d, training accuracy %g"%(i, train_accuracy)
-
+      train_accuracy = accuracy.eval(feed_dict={ x:batch[0], y_: yTrain, keep_prob: 1.0}, session=sess)
+      print "\n**step %d, training accuracy %g"%(i, train_accuracy)
+               
    train_step.run(feed_dict={x: batch[0], y_: yTrain, keep_prob: 0.8}, session=sess)
+#    if i%5 == 0:
+#       ytmp = (y_conv.eval(feed_dict={x: batch[0], y_: yTrain, keep_prob: 1.0},session=sess))
+#       import ipdb; ipdb.set_trace()
    print "\n***step %d finished, time = %s" %(i, time.time() - step_start)
 
-# Evaluate the prediction
-test = dataset.get_batch(83)
-testLabels = test[1]
-yTest = np.zeros((11, nClasses))
-for j in xrange(11):
-   yTest[j][ int(testLabels[j]) ] = 1
-
-print "Accuracy = "
-print accuracy.eval(feed_dict={x: test[0], y_: yTest, keep_prob: 1.0})
+# Evaluate the final accuracy
+accuracies = []
+for i in xrange(0, dataset.get_size(), batchSize):
+   test = dataset.get_sequential_batch(batchSize, i)
+   testLabels = test[1]
+   
+   returnBathSize = test[0].shape[0] 
+   if returnBathSize > 0:
+      yTest = np.zeros((returnBathSize, nClasses))
+   #    import ipdb; ipdb.set_trace()
+      for j in xrange(returnBathSize):
+         yTest[j][ int(testLabels[j]) ] = 1
+      
+      accuracies.append(accuracy.eval(feed_dict={x: test[0], y_: yTest, keep_prob: 1.0},session=sess))
+         
+print "\n\n****Accuracy = " + str((sum(accuracies) / float(len(accuracies))))
 print("--- %s seconds ---" % (time.time() - start_time))
