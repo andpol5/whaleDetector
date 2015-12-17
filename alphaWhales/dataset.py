17736d9fa25b95a61c34696ecb9f1f5c5707a183
andpol5 <and.pol5@gmail.com>
2015-12-18 00:07:35 +0100

    Fix spaces in dataset

--- alphaWhales/dataset.py
+++ alphaWhales/dataset.py
@@ -14,106 +14,106 @@ from scipy import random
 
 class DataSet(object):
 
-  def __init__(self, inputDir, images, labels):
-      self.indexInEpoch = 0
-      self.epochsCompleted = 0
-      self.images = images
-      self.labels = labels
-      self.inputDir = inputDir
+   def __init__(self, inputDir, images, labels):
+         self.indexInEpoch = 0
+         self.epochsCompleted = 0
+         self.images = images
+         self.labels = labels
+         self.inputDir = inputDir
 
-  @property
-  def num_examples(self):
-    return len(self.images)
+   @property
+   def num_examples(self):
+      return len(self.images)
 
-  @property
-  def epochs_completed(self):
-    return self.epochsCompleted
+   @property
+   def epochs_completed(self):
+      return self.epochsCompleted
 
-  def getAll(self):
-      return self.read_images([os.path.join(self.inputDir, x) for x in self.images]), self.labels
+   def getAll(self):
+         return self.read_images([os.path.join(self.inputDir, x) for x in self.images]), self.labels
 
-  def get_random_batch(self, batchSize):
-    randInd = random.permutation(len(self.images))[:batchSize]
-    return self.read_images([os.path.join(self.inputDir, x) for x in self.images[randInd]]), self.labels[randInd]
+   def get_random_batch(self, batchSize):
+      randInd = random.permutation(len(self.images))[:batchSize]
+      return self.read_images([os.path.join(self.inputDir, x) for x in self.images[randInd]]), self.labels[randInd]
 
-  def get_sequential_batch(self, batchSize):
-    start = self.indexInEpoch
-    self.indexInEpoch += batchSize
-    if self.indexInEpoch > self.num_examples:
-       # Finished epoch
-       self.epochsCompleted += 1
-       # Shuffle the data
-       perm = np.arange(self.num_examples)
-       np.random.shuffle(perm)
-       self.images = self.images[perm]
-       self.labels = self.labels[perm]
-       # Start next epoch
-       start = 0
-       self.indexInEpoch = batchSize
-       assert batchSize <= self.num_examples
-    end = self.indexInEpoch
-    return self.read_images([os.path.join(self.inputDir, x) for x in self.images[start:end]]), self.labels[start:end]
+   def get_sequential_batch(self, batchSize):
+      start = self.indexInEpoch
+      self.indexInEpoch += batchSize
+      if self.indexInEpoch > self.num_examples:
+         # Finished epoch
+         self.epochsCompleted += 1
+         # Shuffle the data
+         perm = np.arange(self.num_examples)
+         np.random.shuffle(perm)
+         self.images = self.images[perm]
+         self.labels = self.labels[perm]
+         # Start next epoch
+         start = 0
+         self.indexInEpoch = batchSize
+         assert batchSize <= self.num_examples
+      end = self.indexInEpoch
+      return self.read_images([os.path.join(self.inputDir, x) for x in self.images[start:end]]), self.labels[start:end]
 
-  def read_images(self, filenames):
-    images = []
+   def read_images(self, filenames):
+      images = []
 
-    for file in filenames:
-      im = cv2.imread(file, flags=cv2.IMREAD_GRAYSCALE)
-      images.append(im.flatten())
-    return np.asarray(images)
+      for file in filenames:
+         im = cv2.imread(file, flags=cv2.IMREAD_GRAYSCALE)
+         images.append(im.flatten())
+      return np.asarray(images)
 
 class DataSets(object):
-    def __init__(self, inputDir, labelsFile):
-        
-        self.inputDir = inputDir
-        output = np.genfromtxt(labelsFile, skip_header=1, dtype=[('image', 'S10'), ('label', 'S11')], delimiter=',')
-    
-        labels = [x[1] for x in output]
-    
-        self.numberOfClasses = len(set(labels))
-        self.filenames = []
-      
-        for file in os.listdir(inputDir):
-           if(file.endswith('.jpg')):
-              self.filenames.append(file)
-    
-        self.filenames = np.array(self.filenames)
-        self.labelsDict = {int(re.search("w_(\\d+)\.jpg", x[0]).group(1)):int(re.search("whale_(\\d+)", x[1]).group(1)) for x in output}
-    
-        # to provide default values
-        self.labelsDict = defaultdict(lambda:0, self.labelsDict)
-    
-        self.examples = [int(re.search("w_(\\d+).jpg",x).group(1)) for x in self.filenames]
-        self.allLabels = [self.labelsDict[x] for x in self.examples]
-        self.allLabels = np.array(self.allLabels)
-    
-        self.imagesIds = np.array(self.examples)
-        self.images = self.filenames[self.allLabels > 0]
-        self.origLabels = self.allLabels[self.allLabels > 0]
-    
-        l = np.zeros(max(self.origLabels))
-        i = 0
-        for k in sorted(set(self.origLabels)):
-           l[k-1] = i
-           i += 1
-        self.labels = np.array([l[x-1] for x in self.origLabels])
-
-        size = len(self.images)
-        self.valInd = range(size-200-1, size-1) #random.permutation(len(self.images))[:20]
-        self.trainInd = range(0, size-200-2)
+      def __init__(self, inputDir, labelsFile):
             
-        self.trainImages = self.images[self.trainInd]
-        self.trainLabels = self.labels[self.trainInd]
-        
-        self.valImages = self.images[self.valInd]
-        self.valLabels = self.labels[self.valInd]
-    
+            self.inputDir = inputDir
+            output = np.genfromtxt(labelsFile, skip_header=1, dtype=[('image', 'S10'), ('label', 'S11')], delimiter=',')
+      
+            labels = [x[1] for x in output]
+      
+            self.numberOfClasses = len(set(labels))
+            self.filenames = []
+         
+            for file in os.listdir(inputDir):
+               if(file.endswith('.jpg')):
+                  self.filenames.append(file)
+      
+            self.filenames = np.array(self.filenames)
+            self.labelsDict = {int(re.search("w_(\\d+)\.jpg", x[0]).group(1)):int(re.search("whale_(\\d+)", x[1]).group(1)) for x in output}
+      
+            # to provide default values
+            self.labelsDict = defaultdict(lambda:0, self.labelsDict)
+      
+            self.examples = [int(re.search("w_(\\d+).jpg",x).group(1)) for x in self.filenames]
+            self.allLabels = [self.labelsDict[x] for x in self.examples]
+            self.allLabels = np.array(self.allLabels)
+      
+            self.imagesIds = np.array(self.examples)
+            self.images = self.filenames[self.allLabels > 0]
+            self.origLabels = self.allLabels[self.allLabels > 0]
+      
+            l = np.zeros(max(self.origLabels))
+            i = 0
+            for k in sorted(set(self.origLabels)):
+               l[k-1] = i
+               i += 1
+            self.labels = np.array([l[x-1] for x in self.origLabels])
+
+            size = len(self.images)
+            self.valInd = range(size-200-1, size-1) #random.permutation(len(self.images))[:20]
+            self.trainInd = range(0, size-200-2)
+                  
+            self.trainImages = self.images[self.trainInd]
+            self.trainLabels = self.labels[self.trainInd]
+            
+            self.valImages = self.images[self.valInd]
+            self.valLabels = self.labels[self.valInd]
+      
 
 def read_data_sets(trainDir, labelsFile):
 
-  data_sets = DataSets(trainDir, labelsFile)
+   data_sets = DataSets(trainDir, labelsFile)
 
-  data_sets.train = DataSet(trainDir, data_sets.trainImages, data_sets.trainLabels)
-  data_sets.validation = DataSet(trainDir, data_sets.valImages, data_sets.valLabels)
+   data_sets.train = DataSet(trainDir, data_sets.trainImages, data_sets.trainLabels)
+   data_sets.validation = DataSet(trainDir, data_sets.valImages, data_sets.valLabels)
 
-  return data_sets
+   return data_sets
