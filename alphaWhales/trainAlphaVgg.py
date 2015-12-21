67159eababf3717422b441e2eaed478e737f9801
mira <ilmirashaim@gmail.com>
2015-12-21 17:44:46 +0100

    network twice smaller

--- alphaWhales/trainAlphaVgg.py
+++ alphaWhales/trainAlphaVgg.py
@@ -69,13 +69,13 @@ start_time = time.time()
 # Force CPU only mode
 with tf.device('/cpu:0'):
    # Creates a session with log_device_placement set to True.
-   sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
-   # sess = tf.Session()
+   # sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+   sess = tf.Session()
 
    # Constants
    nClasses = 38
    imageSize = 256*256
-   batchSize = 50
+   batchSize = 10
 
    # The size of the images is 256x256`
    x = tf.placeholder("float", shape=[None, imageSize], name="Input")
@@ -83,10 +83,10 @@ with tf.device('/cpu:0'):
    y_ = tf.placeholder("float", shape=[None, nClasses], name = "Output")
 
    # FIRST SUBLAYER (64 features, 2 convolutions)
-   w_conv11 = weight_variable([3, 3, 1, 64], name="Weights_conv11")
-   b_conv11 = bias_variable([64], name="b_conv11")
-   w_conv12 = weight_variable([3, 3, 64, 64], name="Weights_conv12")
-   b_conv12 = bias_variable([64], name="b_conv12")
+   w_conv11 = weight_variable([3, 3, 1, 32], name="Weights_conv11")
+   b_conv11 = bias_variable([32], name="b_conv11")
+   w_conv12 = weight_variable([3, 3, 32, 32], name="Weights_conv12")
+   b_conv12 = bias_variable([32], name="b_conv12")
 
    # To apply the layer, we first reshape x to a 4d tensor, with the second
    # and third dimensions corresponding to image width and height,
@@ -100,22 +100,22 @@ with tf.device('/cpu:0'):
    h_pool1 = max_pool_2x2(h_conv12, name="pool1")
 
    # SECOND SUBLAYER (128 features, 2 convolutions)
-   w_conv21 = weight_variable([3, 3, 64, 128], name="Weights_conv21")
-   b_conv21 = bias_variable([128], name="b_conv21")
-   w_conv22 = weight_variable([3, 3, 128, 128], name="Weights_conv22")
-   b_conv22 = bias_variable([128], name="b_conv22")
+   w_conv21 = weight_variable([3, 3, 32, 64], name="Weights_conv21")
+   b_conv21 = bias_variable([64], name="b_conv21")
+   w_conv22 = weight_variable([3, 3, 64, 64], name="Weights_conv22")
+   b_conv22 = bias_variable([64], name="b_conv22")
 
    h_conv21 = tf.nn.relu(conv2d(h_pool1, w_conv21)  + b_conv21)
    h_conv22 = tf.nn.relu(conv2d(h_conv21, w_conv22) + b_conv22)
    h_pool2 = max_pool_2x2(h_conv22, name="pool2")
 
    # THIRD SUBLAYER (256 features, 3 convolutions)
-   w_conv31 = weight_variable([3, 3, 128, 256], name="Weights_conv31")
-   b_conv31 = bias_variable([256], name="b_conv31")
-   w_conv32 = weight_variable([3, 3, 256, 256], name="Weights_conv32")
-   b_conv32 = bias_variable([256], name="b_conv32")
-   w_conv33 = weight_variable([3, 3, 256, 256], name="Weights_conv33")
-   b_conv33 = bias_variable([256], name="b_conv33")
+   w_conv31 = weight_variable([3, 3, 64, 128], name="Weights_conv31")
+   b_conv31 = bias_variable([128], name="b_conv31")
+   w_conv32 = weight_variable([3, 3, 128, 128], name="Weights_conv32")
+   b_conv32 = bias_variable([128], name="b_conv32")
+   w_conv33 = weight_variable([3, 3, 128, 128], name="Weights_conv33")
+   b_conv33 = bias_variable([128], name="b_conv33")
 
    h_conv31 = tf.nn.relu(conv2d(h_pool2, w_conv31)  + b_conv31)
    h_conv32 = tf.nn.relu(conv2d(h_conv31, w_conv32) + b_conv32)
@@ -123,30 +123,18 @@ with tf.device('/cpu:0'):
    h_pool3 = max_pool_2x2(h_conv33, name="pool3")
 
    # FOURTH SUBLAYER (512 features, 3 convolutions)
-   w_conv41 = weight_variable([3, 3, 256, 512], name="Weights_conv41")
-   b_conv41 = bias_variable([512], name="b_conv41")
-   w_conv42 = weight_variable([3, 3, 512, 512], name="Weights_conv42")
-   b_conv42 = bias_variable([512], name="b_conv42")
-   w_conv43 = weight_variable([3, 3, 512, 512], name="Weights_conv43")
-   b_conv43 = bias_variable([512], name="b_conv43")
+   w_conv41 = weight_variable([3, 3, 128, 256], name="Weights_conv41")
+   b_conv41 = bias_variable([256], name="b_conv41")
+   w_conv42 = weight_variable([3, 3, 256, 256], name="Weights_conv42")
+   b_conv42 = bias_variable([256], name="b_conv42")
+   w_conv43 = weight_variable([3, 3, 256, 256], name="Weights_conv43")
+   b_conv43 = bias_variable([256], name="b_conv43")
 
    h_conv41 = tf.nn.relu(conv2d(h_pool3, w_conv41)  + b_conv41)
    h_conv42 = tf.nn.relu(conv2d(h_conv41, w_conv42) + b_conv42)
    h_conv43 = tf.nn.relu(conv2d(h_conv42, w_conv43) + b_conv43)
    h_pool4 = max_pool_2x2(h_conv43, name="pool4")
 
-   # FIFTH SUBLAYER (512 features, 3 convolutions)
-   w_conv51 = weight_variable([3, 3, 512, 512], name="Weights_conv51")
-   b_conv51 = bias_variable([512], name="b_conv51")
-   w_conv52 = weight_variable([3, 3, 512, 512], name="Weights_conv52")
-   b_conv52 = bias_variable([512], name="b_conv52")
-   w_conv53 = weight_variable([3, 3, 512, 512], name="Weights_conv53")
-   b_conv53 = bias_variable([512], name="b_conv53")
-
-   h_conv51 = tf.nn.relu(conv2d(h_pool4, w_conv51)  + b_conv51)
-   h_conv52 = tf.nn.relu(conv2d(h_conv51, w_conv52) + b_conv52)
-   h_conv53 = tf.nn.relu(conv2d(h_conv52, w_conv53) + b_conv53)
-   h_pool5 = max_pool_2x2(h_conv53, name="pool5")
 
    # DENSELY CONNECTED LAYER
    # Now that the image size has been reduced to 8x8,
@@ -160,23 +148,23 @@ with tf.device('/cpu:0'):
    # FC:           [1x1x38]        fc3 (to output)
 
    # Fully connected layer 1 (4096 neurons)
-   w_fc1 = weight_variable([8*8*512, 4096], name="Weights_fc1")
-   b_fc1 = bias_variable([4096], name="biases_fc1")
+   w_fc1 = weight_variable([16*16*256, 2048], name="Weights_fc1")
+   b_fc1 = bias_variable([2048], name="biases_fc1")
 
-   h_pool5_flat = tf.reshape(h_pool5, [-1, 8*8*512])
+   h_pool5_flat = tf.reshape(h_pool4, [-1, 16*16*256])
    h_fc1 = tf.nn.relu(tf.matmul(h_pool5_flat, w_fc1) + b_fc1)
    # Dropout of fc1 (dropout keep probability of 0.5)
    keep_prob = tf.placeholder("float")
    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
    # Fully connected layer 2
-   w_fc2 = weight_variable([4096, 4096], name="Weights_fc2")
-   b_fc2 = bias_variable([4096], name="biases_fc2")
+   w_fc2 = weight_variable([2048, 2048], name="Weights_fc2")
+   b_fc2 = bias_variable([2048], name="biases_fc2")
    h_fc2 = tf.nn.relu(tf.matmul(h_fc1_drop, w_fc2) + b_fc2)
    h_fc2_drop = tf.nn.dropout(h_fc2, keep_prob)
 
    # Readout layer
-   w_fc3 = weight_variable([4096, nClasses], name="Weights_fc3")
+   w_fc3 = weight_variable([2048, nClasses], name="Weights_fc3")
    b_fc3 = bias_variable([nClasses], name="biases_fc3")
    y_conv = tf.nn.softmax(tf.matmul(h_fc2_drop, w_fc3) + b_fc3)
 
@@ -209,7 +197,7 @@ with tf.device('/cpu:0'):
          yTrain[j-1][ int(labels[j-1]) ] = 1
       train_step.run(feed_dict={x: batch[0], y_: yTrain, keep_prob:0.5}, session=sess)
 
-      if i%25 == 0 and i != 0:
+      if i%5 == 0 and i != 0:
          #evaluate accuracy on random batch of 100 samples
          batch = datasets.train.get_random_batch(100)
          labels = batch[1]
@@ -226,7 +214,7 @@ with tf.device('/cpu:0'):
          f1.flush()
          summary_writer.add_summary(summary_str, i)
 
-      f1.write("\nstep %d finished, %d seconds \n" % (i, time.time() - start_time))
+      f1.write("\nstep %d finished, %d seconds \n" % (i, time.time() - step_start))
       f1.flush()
 
    # Evaluate the prediction
